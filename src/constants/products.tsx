433fb9eb53e2d32c76e07086b32e7a7ca9c34a1b
Tyler Wade <vaxonreal@gmail.com>
2024-12-04 16:59:11 -0500

    images updated. Added Hayden Hester site to projects

--- src/constants/products.tsx
+++ src/constants/products.tsx
@@ -1,6 +1,12 @@
-import guidedGospelThumbnail from "/public/images/guidedThumbnail.png";
-import guidedGospelImage1 from "/public/images/guidedImage1.png";
-import guidedGospelImage2 from "/public/images/guidedImage2.png";
+import guidedGospelThumbnail from "/public/images/products/guided-thumbnail.png";
+import guidedGospelImage1 from "/public/images/products/guided1.png";
+import guidedGospelImage2 from "/public/images/products/guided2.png";
+import guidedGospelImage3 from "/public/images/products/guided3.png";
+
+import haydenThumbnail from "/public/images/products/hayden-thumbnail.png";
+import haydenImage1 from "/public/images/products/hayden1.png";
+import haydenImage2 from "/public/images/products/hayden2.png";
+import haydenImage3 from "/public/images/products/hayden3.png";
 
 
 export const products = [
@@ -8,9 +14,9 @@ export const products = [
     href: "https://guidedgospel.net",
     title: "Guided Gospel",
     description:
-      "A Christian AI Based Resource Hub that provides users with Faith-Based guidance.",
+      "A Christian AI Based Resource Hub that provides users with Faith-Based guidance. AI ChatBot, Christian Social Media, Mobile Bible all in one!",
     thumbnail: guidedGospelThumbnail,
-    images: [guidedGospelImage1, guidedGospelThumbnail, guidedGospelImage2],
+    images: [guidedGospelImage1, guidedGospelImage2, guidedGospelImage3],
     stack: ["TypeScript", "React", "Node", "Express", "MySQL", "Tailwind"],
     slug: "guidedgospel",
     content: (
@@ -20,6 +26,29 @@ export const products = [
         </p>
         <p>
           With Faith Guidance at your fingertips, Get answers to your biggest questions within seconds. Guided delivers relevant Bible verses and interpretations instantly, helping you find clarity and guidance without delay. Receive responses crafted to your specific queries and spiritual needs, ensuring that the guidance you get is meaningful and relevant.
+        </p>{" "}
+        <p>
+          Share your testimony to others with Guided Together, coming to release soon!
+        </p>{" "}
+      </div>
+    ),
+  },
+  {
+    href: "https://haydenhester.tylerwade.net/",
+    title: "Hayden Hester - Musician",
+    description:
+      "A Frontend website for the musical artist, Hayden Hester.",
+    thumbnail: haydenThumbnail,
+    images: [haydenImage1, haydenImage2, haydenImage3],
+    stack: ["HTML", "CSS", "JavaScript"],
+    slug: "haydenhester",
+    content: (
+      <div>
+        <p>
+          This project serves as an example design for the artist Hayden Hester. <em>The music playback functionality is not intended to function</em>.{" "}
+        </p>
+        <p>
+          A rising star from the heart of Nashville, Hayden Hester combines soulful melodies with raw, heartfelt lyrics that captivate audiences everywhere. With a style rooted in pop and infused with modern flair, Hayden’s music tells stories of life, love, and resilience. Whether on stage or in the studio, Hayden’s passion for music shines through every note, making him one to watch in the music scene.
         </p>{" "}
       </div>
     ),
